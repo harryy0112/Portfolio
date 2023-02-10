84214b791f2fde0090007b9093fdba60344ec412
Hardik Agarwal <agarwalhardik562@gmail.com>
2023-02-11 02:10:30 +0530

    Day1

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,10 +1,16 @@
-import React from 'react'
-import './header.css'
+import React from "react";
+import "./header.css";
 
 const Header = () => {
   return (
-    <div>Header</div>
-  )
-}
+    <header>
+      <div className="container header__container">
+        <h5>Hello I'm</h5>
+        <h1>Hardik Agarwal</h1>
+        <h5 className="text-light">Fullstack Developer</h5>
+      </div>
+    </header>
+  );
+};
 
-export default Header
+export default Header;
