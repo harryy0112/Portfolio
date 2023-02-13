690eed7d0f50e63810dc137d4d402b981d52cc81
Hardik Agarwal <agarwalhardik562@gmail.com>
2023-02-13 19:30:09 +0530

    Day3

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,5 +1,8 @@
 import React from "react";
 import "./header.css";
+import CTA from "./CTA";
+import ME from "../../assets/me.png";
+import HeaderSocials from "./HeaderSocials";
 
 const Header = () => {
   return (
@@ -8,6 +11,16 @@ const Header = () => {
         <h5>Hello I'm</h5>
         <h1>Hardik Agarwal</h1>
         <h5 className="text-light">Fullstack Developer</h5>
+        <CTA />
+        <HeaderSocials />
+
+        <div className="me">
+          <img src={ME} alt="me" />
+        </div>
+
+        <a href="#contact" className="scroll_down">
+          Scroll Down
+        </a>
       </div>
     </header>
   );
