c320fcb7526baeea896eddfbada8c44e88987bdd
Hardik Agarwal <agarwalhardik562@gmail.com>
2023-02-21 02:31:41 +0530

    Day-4

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -8,7 +8,7 @@ const Header = () => {
   return (
     <header>
       <div className="container header__container">
-        <h5>Hello I'm</h5>
+        <h5 className="hello">Hello I'm</h5>
         <h1>Hardik Agarwal</h1>
         <h5 className="text-light">Fullstack Developer</h5>
         <CTA />
@@ -18,7 +18,7 @@ const Header = () => {
           <img src={ME} alt="me" />
         </div>
 
-        <a href="#contact" className="scroll_down">
+        <a href="#contact" className="scroll__down">
           Scroll Down
         </a>
       </div>
