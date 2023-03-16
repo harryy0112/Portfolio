6e442b98184fc5f41bd68c06b3b5c2e80f88c17a
Hardik Agarwal <agarwalhardik562@gmail.com>
2023-03-16 21:48:07 +0530

    updates

--- src/components/header/CTA.jsx
+++ src/components/header/CTA.jsx
@@ -1,11 +1,15 @@
 import React from "react";
-import CV from '../../assets/cv.pdf'
+import HardikResume from "../../assets/HardikResume.pdf";
 
 const CTA = () => {
   return (
     <div className="cta">
-      <a href={CV} download className="btn">Download CV</a>
-      <a href="#contact" className="btn btn-primary">Let's Talk</a>
+      <a href={HardikResume} download className="btn">
+        Download CV
+      </a>
+      <a href="#contact" className="btn btn-primary">
+        Let's Talk
+      </a>
     </div>
   );
 };
